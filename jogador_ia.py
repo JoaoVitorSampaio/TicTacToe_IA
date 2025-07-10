e62697af7aab27b8a27708e26854cf5310b2fe4b
João Vitor Sampaio <joaovitorsampai@gmail.com>
2025-07-09 23:49:48 -0300

    improvements

--- jogador_ia.py
+++ jogador_ia.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from random import randint
+from random import choice
 from typing import Optional, Tuple
 
 from jogador import Jogador
@@ -8,110 +8,89 @@ from tabuleiro import Tabuleiro
 class JogadorIA(Jogador):
     def __init__(self, tabuleiro: Tabuleiro, tipo: int):
         super().__init__(tabuleiro, tipo)
+        self.oponente = Tabuleiro.JOGADOR_X if tipo == Tabuleiro.JOGADOR_0 else Tabuleiro.JOGADOR_0
 
     def getJogada(self) -> Optional[Tuple[int, int]]:
         matriz = self.tabuleiro.matriz
 
-        lista = []
-        for l in range(3):
-            for c in range(3):
-                if matriz[l][c] == Tabuleiro.DESCONHECIDO:
-                    lista.append((l, c))
+        # Regra R1: Bloqueio ou vitória imediata
+        jogada = self.verifica_dupla(self.tipo)
+        if jogada:
+            return jogada
+        jogada = self.verifica_dupla(self.oponente)
+        if jogada:
+            return jogada
 
-        # R1: Se houver linha, coluna ou diagonal com duas marcas e uma casa vazia
-        for emptySpot in lista:
-            x, y = emptySpot
+        # Regra R2: Criar duas duplas ao mesmo tempo
+        jogada = self.criar_duplas()
+        if jogada:
+            return jogada
 
-            # Horizontal
-            row = [matriz[x][i] for i in range(3)]
-            if (row.count(Tabuleiro.JOGADOR_0) == 2 or row.count(Tabuleiro.JOGADOR_X) == 2) and \
-                row.count(Tabuleiro.DESCONHECIDO) == 1:
-                return emptySpot
-
-            # Vertical
-            col = [matriz[i][y] for i in range(3)]
-            if (col.count(Tabuleiro.JOGADOR_0) == 2 or col.count(Tabuleiro.JOGADOR_X) == 2) and \
-                col.count(Tabuleiro.DESCONHECIDO) == 1:
-                return emptySpot
-
-            # Diagonal principal
-            if x == y:
-                diag = [matriz[i][i] for i in range(3)]
-                if (diag.count(Tabuleiro.JOGADOR_0) == 2 or diag.count(Tabuleiro.JOGADOR_X) == 2) and \
-                    diag.count(Tabuleiro.DESCONHECIDO) == 1:
-                    return emptySpot
-
-            # Diagonal secundária
-            if x + y == 2:
-                diag = [matriz[i][2 - i] for i in range(3)]
-                if (diag.count(Tabuleiro.JOGADOR_0) == 2 or diag.count(Tabuleiro.JOGADOR_X) == 2) and \
-                    diag.count(Tabuleiro.DESCONHECIDO) == 1:
-                    return emptySpot
-
-        # R2: Se houver uma jogada que crie duas possibilidades de vitória (duas ameaças)
-        for emptySpot in lista:
-            x, y = emptySpot
-
-            # Testa colocar a peça temporariamente
-            matriz[x][y] = Tabuleiro.JOGADOR_0
-
-            threats = 0
-
-            # Conta se há linhas com 2 peças e 1 espaço livre após a jogada
-
-            # Horizontal
-            row = [matriz[x][i] for i in range(3)]
-            if row.count(Tabuleiro.JOGADOR_0) == 2 and row.count(Tabuleiro.DESCONHECIDO) == 1:
-                threats += 1
-
-            # Vertical
-            col = [matriz[i][y] for i in range(3)]
-            if col.count(Tabuleiro.JOGADOR_0) == 2 and col.count(Tabuleiro.DESCONHECIDO) == 1:
-                threats += 1
-
-            # Diagonal principal
-            if x == y:
-                diag = [matriz[i][i] for i in range(3)]
-                if diag.count(Tabuleiro.JOGADOR_0) == 2 and diag.count(Tabuleiro.DESCONHECIDO) == 1:
-                    threats += 1
-
-            # Diagonal secundária
-            if x + y == 2:
-                diag = [matriz[i][2 - i] for i in range(3)]
-                if diag.count(Tabuleiro.JOGADOR_0) == 2 and diag.count(Tabuleiro.DESCONHECIDO) == 1:
-                    threats += 1
-
-            # Desfaz a jogada temporária
-            matriz[x][y] = Tabuleiro.DESCONHECIDO
-
-            if threats >= 2:
-                return emptySpot
-
-        # R3: Se o centro estiver livre
+        # R3: Centro
         if matriz[1][1] == Tabuleiro.DESCONHECIDO:
             return (1, 1)
 
-        # R4: Se oponente marcou canto, marcar canto oposto
-        if matriz[0][0] == Tabuleiro.JOGADOR_X and matriz[2][2] == Tabuleiro.DESCONHECIDO:
-            return (2, 2)
-        if matriz[0][2] == Tabuleiro.JOGADOR_X and matriz[2][0] == Tabuleiro.DESCONHECIDO:
-            return (2, 0)
-        if matriz[2][0] == Tabuleiro.JOGADOR_X and matriz[0][2] == Tabuleiro.DESCONHECIDO:
-            return (0, 2)
-        if matriz[2][2] == Tabuleiro.JOGADOR_X and matriz[0][0] == Tabuleiro.DESCONHECIDO:
-            return (0, 0)
+        # R4: Canto oposto
+        cantos_opostos = [((0, 0), (2, 2)), ((0, 2), (2, 0)), ((2, 0), (0, 2)), ((2, 2), (0, 0))]
+        for (a, b) in cantos_opostos:
+            if matriz[a[0]][a[1]] == self.oponente and matriz[b[0]][b[1]] == Tabuleiro.DESCONHECIDO:
+                return b
 
-        # R5: Marcar um canto vazio
-        lista_de_cantos = [(0, 0), (2, 2), (0, 2), (2, 0)]
-        cantos_vazios = [pos for pos in lista_de_cantos if matriz[pos[0]][pos[1]] == Tabuleiro.DESCONHECIDO]
+        # R5: Qualquer canto
+        cantos = [(0, 0), (0, 2), (2, 0), (2, 2)]
+        cantos_livres = [pos for pos in cantos if matriz[pos[0]][pos[1]] == Tabuleiro.DESCONHECIDO]
+        if cantos_livres:
+            return choice(cantos_livres)
 
-        if cantos_vazios:
-            return cantos_vazios[randint(0, len(cantos_vazios)-1)]
+        # R6: Qualquer espaço livre
+        for l in range(3):
+            for c in range(3):
+                if matriz[l][c] == Tabuleiro.DESCONHECIDO:
+                    return (l, c)
+        return None
 
-        # R6: Escolher posição aleatória
-        if lista:
-            return lista[randint(0, len(lista)-1)]
+    def verifica_dupla(self, jogador: int) -> Optional[Tuple[int, int]]:
+        matriz = self.tabuleiro.matriz
+
+        for i in range(3):
+            # Linhas
+            linha = matriz[i]
+            if linha.count(jogador) == 2 and linha.count(Tabuleiro.DESCONHECIDO) == 1:
+                return (i, linha.index(Tabuleiro.DESCONHECIDO))
+
+            # Colunas
+            coluna = [matriz[0][i], matriz[1][i], matriz[2][i]]
+            if coluna.count(jogador) == 2 and coluna.count(Tabuleiro.DESCONHECIDO) == 1:
+                return (coluna.index(Tabuleiro.DESCONHECIDO), i)
+
+        # Diagonal principal
+        diag1 = [matriz[i][i] for i in range(3)]
+        if diag1.count(jogador) == 2 and diag1.count(Tabuleiro.DESCONHECIDO) == 1:
+            idx = diag1.index(Tabuleiro.DESCONHECIDO)
+            return (idx, idx)
+
+        # Diagonal secundária
+        diag2 = [matriz[i][2 - i] for i in range(3)]
+        if diag2.count(jogador) == 2 and diag2.count(Tabuleiro.DESCONHECIDO) == 1:
+            idx = diag2.index(Tabuleiro.DESCONHECIDO)
+            return (idx, 2 - idx)
 
         return None
-        # Se não houver jogada possível, retorna None
-        # Isso pode acontecer se o tabuleiro estiver cheio ou se não houver jogadas válidas
+
+    def criar_duplas(self) -> Optional[Tuple[int, int]]:
+        matriz = self.tabuleiro.matriz
+        possibilidades = []
+
+        for l in range(3):
+            for c in range(3):
+                if matriz[l][c] == Tabuleiro.DESCONHECIDO:
+                    # Testa jogada temporariamente
+                    matriz[l][c] = self.tipo
+                    dupla = self.verifica_dupla(self.tipo)
+                    matriz[l][c] = Tabuleiro.DESCONHECIDO
+                    if dupla:
+                        possibilidades.append((l, c))
+
+        if possibilidades:
+            return choice(possibilidades)
+        return None
--- tabuleiro.py
+++ tabuleiro.py
@@ -1,49 +1,51 @@
 # -*- coding: utf-8 -*-
 
+
 class Tabuleiro:
     DESCONHECIDO = 0
     JOGADOR_0 = 1
     JOGADOR_X = 4
 
+
     def __init__(self):
-        self.matriz = [
-            [Tabuleiro.DESCONHECIDO for _ in range(3)]
-            for _ in range(3)
-        ]
+        self.matriz = [ [Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO],
+                        [Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO],
+                        [Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO]]
+       
+       
+    def tem_campeao(self):
+        # Verifica linhas
+        for linha in self.matriz:
+            soma = sum(linha)
+            if soma == 3:
+                print("Jogador 0 ganhou!")
+                return Tabuleiro.JOGADOR_0
+            elif soma == 12:
+                print("Jogador X ganhou!")
+                return Tabuleiro.JOGADOR_X
 
-    def tem_campeao(self, debug=False):
-        if debug:
-            print("\ntabuleiro:")
-            for linha in self.matriz:
-                print(linha)
 
-        # Verificar linhas e colunas
+        # Verifica colunas
         for i in range(3):
-            # Linhas
-            if sum(self.matriz[i]) == Tabuleiro.JOGADOR_0 * 3:
+            soma = self.matriz[0][i] + self.matriz[1][i] + self.matriz[2][i]
+            if soma == 3:
+                print("Jogador 0 ganhou!")
                 return Tabuleiro.JOGADOR_0
-            if sum(self.matriz[i]) == Tabuleiro.JOGADOR_X * 3:
+            elif soma == 12:
+                print("Jogador X ganhou!")
                 return Tabuleiro.JOGADOR_X
 
-            # Colunas
-            soma_coluna = sum(self.matriz[j][i] for j in range(3))
-            if soma_coluna == Tabuleiro.JOGADOR_0 * 3:
-                return Tabuleiro.JOGADOR_0
-            if soma_coluna == Tabuleiro.JOGADOR_X * 3:
-                return Tabuleiro.JOGADOR_X
 
-        # Diagonal principal
-        diag_principal = sum(self.matriz[i][i] for i in range(3))
-        if diag_principal == Tabuleiro.JOGADOR_0 * 3:
+        # Verifica diagonais
+        diagonal1 = self.matriz[0][0] + self.matriz[1][1] + self.matriz[2][2]
+        diagonal2 = self.matriz[0][2] + self.matriz[1][1] + self.matriz[2][0]
+        if diagonal1 == 3 or diagonal2 == 3:
+            print("Jogador 0 ganhou!")
             return Tabuleiro.JOGADOR_0
-        if diag_principal == Tabuleiro.JOGADOR_X * 3:
+        elif diagonal1 == 12 or diagonal2 == 12:
+            print("Jogador X ganhou!")
             return Tabuleiro.JOGADOR_X
 
-        # Diagonal secundária
-        diag_secundaria = sum(self.matriz[i][2 - i] for i in range(3))
-        if diag_secundaria == Tabuleiro.JOGADOR_0 * 3:
-            return Tabuleiro.JOGADOR_0
-        if diag_secundaria == Tabuleiro.JOGADOR_X * 3:
-            return Tabuleiro.JOGADOR_X
 
+        # Nenhum vencedor
         return Tabuleiro.DESCONHECIDO
